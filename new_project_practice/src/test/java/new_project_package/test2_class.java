0bd18f9a47fa44363d66ffeea65c7b1f41302c78
RichieWilli <richardwilliam1994@gmail.com>
2023-08-05 15:22:09 +0530

    4th commit

---
+++ new_project_practice/src/test/java/new_project_package/test2_class.java
@@ -0,0 +1,12 @@
+package new_project_package;
+
+import org.testng.annotations.Test;
+
+public class test2_class {
+
+	@Test
+	public void testclass()
+	{
+		System.out.println("hi");
+	}
+}
---
+++ new_project_practice/src/test/java/new_project_package/test_class.java
@@ -0,0 +1,12 @@
+package new_project_package;
+
+import org.testng.annotations.Test;
+
+public class test_class {
+
+	@Test
+	public void testclass()
+	{
+		System.out.println("hii");
+	}
+}
