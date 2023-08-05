a04c67c919a25c27d6259cf74172ac44808a550e
RichieWilli <richardwilliam1994@gmail.com>
2023-08-05 17:14:09 +0530

    check

---
+++ new_project_practice/src/test/java/new_project_package/demo.java
@@ -0,0 +1,19 @@
+package new_project_package;
+
+import org.testng.annotations.Test;
+
+public class demo {
+
+	@Test
+	public void testclass()
+	{
+		System.out.println("hii");
+	}
+
+@Test
+public void democlass()
+{
+System.out.println("hoo");	
+}
+
+}
---
+++ new_project_practice/src/test/java/new_project_package/test_class1.java
@@ -0,0 +1,15 @@
+package new_project_package;
+
+import org.testng.annotations.Test;
+
+public class test_class2 {
+
+	
+	@Test
+	public void testclass2()
+	{
+	System.out.println("Hii");	
+	}
+
+
+}
